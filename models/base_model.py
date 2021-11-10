61ebfe04de37dfa5760e92cdbe81c8ed73428b06
Afonne-CID <delightinbusiness@gmail.com>
2021-11-10 10:43:41 -0800

    modified

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python3
-"""The `BaseModel class` that defines all common attributes/methods for other clasess
+"""The `BaseModel class` that defines all common attributes/methods
+for other classes
 """
+import uuid
+from datetime import datetime
 
+
+class BaseModel:
+    """Defines the common attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel class
+
+        Args:
+            *args (any type): A variable number of arguments
+            **kwargs (dict): Key/value pairs of attributes
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        """Updates the public instance attribute `updated_at`
+        with the current datetime
+        """
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values
+        of `__dict__` of the instance.
+        """
+        converted = self.__dict__.copy()
+        converted["created_at"] = self.created_at.isoformat()
+        converted["updated_at"] = self.updated_at.isoformat()
+        converted["__class__"] = self.__class__.__name__
+        return (converted)
+
+    def __str__(self):
+        """Returns the string representation of an instance of BaseModel
+        """
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
