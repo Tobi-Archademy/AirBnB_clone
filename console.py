f4ca798718fa00811a7e8d8cd9a728d4b35bcecb
Afonne-CID <delightinbusiness@gmail.com>
2021-11-10 05:37:17 -0800

    edit method names

--- console.py
+++ console.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-""" Command interpreter to manager the AirBnB objects
+""" Command interpreter (the `Console`) to manager the AirBnB objects
 """
-import cmd, sys
-import string
+import cmd
 
 
-class BaseModel(cmd.Cmd):
+class Console(cmd.Cmd):
     """Represents the Base model for all other classes in the project
+
+    Attributes:
+        prompt (str): Displays a `(hbnb`) prompt and waits for a command
     """
-    prompt = '(hbnb) '
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Called when nothing is typed before hitting enter
+        """
+        pass
 
     def do_EOF(self, arg):
-        """Cleanly exits the program
+        """Cleanly exits the program by the `EOF` signal
         """
+        print("")
         return True
 
     def do_quit(self, arg):
         """Quits the program
         """
-        sys.exit(1)
+        return True
 
 
 if __name__ == "__main__":
-    BaseModel().cmdloop()
+    Console().cmdloop()
