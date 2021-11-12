755e526f88230fd9ae38f1a8949713a8cb6421d7
Afonne-CID <delightinbusiness@gmail.com>
2021-11-12 14:57:20 -0800

    whitespace error

--- models/user.py
+++ models/user.py
@@ -6,15 +6,15 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Inherits from `BaseModel to create a user
-    
+
     Attributes:
-    email (str): to hold email
-    password (str): to hold password
-    first_name (str): to hold name
-    last_name (str): to hold name
+        email (str): to hold email
+        password (str): to hold password
+        first_name (str): to hold name
+        last_name (str): to hold name
 
     """
-    
+
     email = ""
     password = ""
     first_name = ""
